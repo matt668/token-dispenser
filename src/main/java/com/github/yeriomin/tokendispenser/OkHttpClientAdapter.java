63c57034c15853a032f1db45f6234f4c336e8601
Matt <mjgreenhalgh@qinetiq.com>
2019-12-06 15:30:23 +0000

    Ciphers

--- src/main/java/com/github/yeriomin/tokendispenser/OkHttpClientAdapter.java
+++ src/main/java/com/github/yeriomin/tokendispenser/OkHttpClientAdapter.java
@@ -23,11 +23,11 @@ class OkHttpClientAdapter extends HttpClientAdapter {
             .tlsVersions(TlsVersion.TLS_1_2)
             .cipherSuites(
                 CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-                CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
                 CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
                 CipherSuite.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
                 CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-                CipherSuite.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256)
+                CipherSuite.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256,
+                CipherSuite.TLS_DHE_RSA_WITH_CHACHA20_POLY1305_SHA256,)
             .build();
         
         setClient(new OkHttpClient.Builder()
